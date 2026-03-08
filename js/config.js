501515054806385278dd1b672d64acef1ed7ae63
Luis Miguel Follero <follero.luismiguel.noora@gmail.com>
2026-03-08 15:48:17 +0800

    Remove Firebase configuration from config.js
    Removed Firebase configuration details from config.js.

--- js/config.js
+++ js/config.js
@@ -1,12 +1,5 @@
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBiIUWIus8ao45edrWZEt8MEghyVdGMu5g",
-    authDomain: "guidance-4387c.firebaseapp.com",
-    projectId: "guidance-4387c",
-    storageBucket: "guidance-4387c.appspot.com",
-    messagingSenderId: "491147874431",
-    appId: "1:491147874431:web:8a4bf876d7e919ce8b2f4a"
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
